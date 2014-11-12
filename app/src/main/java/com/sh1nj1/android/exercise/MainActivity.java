7885c59cce84f42b92921964522ac8179939870e
Soonoh Jung <soonohjung@mytrum.com>
2014-11-12 11:26:11 +0900

    Introduce to remove actionbar in Activity.
    However it will not work if I use new appcompat-v7 lollipop version(21.0.0).

--- app/src/main/java/com/sh1nj1/android/exercise/MainActivity.java
+++ app/src/main/java/com/sh1nj1/android/exercise/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sh1nj1.android.exercise;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -31,6 +32,11 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            return true;
+        }
+        else if(id == R.id.action_showNoActionbar) {
+            Intent intent = new Intent(this, NoActionBarActivity.class);
+            startActivity(intent);
             return true;
         }
 
